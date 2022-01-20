4a5cfa88bc09f4f478abced119bdbfd988e5f753
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2022-01-20 13:10:48 -0600

    chore: show minified output in analyzer

--- bin/analyze.js
+++ bin/analyze.js
@@ -2,6 +2,7 @@
 
 const path = require('path')
 const fs = require('fs')
+const terser = require('terser')
 const zlib = require('zlib')
 const recursiveReaddir = require('recursive-readdir')
 
@@ -20,19 +21,35 @@ const measure = (bytes) => {
   return `${size} ${unit}`
 }
 
+const minify = async (filePath) => {
+  const source = fs.readFileSync(filePath, { encoding: 'utf-8' })
+  if (filePath.endsWith('.d.ts')) return source
+
+  const minified = await terser.minify(source, {
+    mangle: {
+      toplevel: true,
+      properties: false,
+    },
+  })
+
+  return minified.code
+}
+
 recursiveReaddir(path.resolve(process.cwd(), 'dist')).then((paths) => {
-  ;['index.esm.js', 'index.cjs.js', 'index.d.ts'].forEach((name) => {
+  ;['index.esm.js', 'index.cjs.js', 'index.d.ts'].forEach(async (name) => {
     let size = 0
     let gzip = 0
 
     const ext = name.replace('index', '')
-    paths.forEach((filePath) => {
-      if (filePath.endsWith(ext)) {
-        const code = fs.readFileSync(filePath)
-        size += code.length
-        gzip += zlib.gzipSync(code, { level: 9 }).length
-      }
-    })
+    await Promise.all(
+      paths.map(async (filePath) => {
+        if (filePath.endsWith(ext)) {
+          const code = await minify(filePath)
+          size += code.length
+          gzip += zlib.gzipSync(code, { level: 9 }).length
+        }
+      }),
+    )
 
     console.log(`Created bundle ${blue(name)}: ${green(measure(size))} → ${green(measure(gzip))} (gzipped)`)
   })
