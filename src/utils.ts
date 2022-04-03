2d89af43b64ad4bedabb94688deb764a53a41e78
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2022-04-03 15:26:54 -0500

    feat: add std140 util to pack UBOs

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,35 @@
+import type { Uniform } from './core/Program'
+
+// Pad to 16 byte chunks of 2, 4 (std140 layout)
+const pad2 = (n: number) => n + (n <= 2 ? n % 2 : (4 - (n % 4)) % 4)
+const pad4 = (n: number) => (n < 4 ? 4 : pad2(n))
+
+/**
+ * Packs uniforms into a std140 compliant array buffer.
+ */
+export const std140 = (uniforms: Uniform[], buffer?: Float32Array) => {
+  // Init buffer
+  if (!buffer) {
+    const length = pad4(uniforms.reduce((n: number, u: Uniform) => n + pad2(typeof u === 'number' ? 1 : u.length), 0))
+    buffer = new Float32Array(length)
+  }
+
+  // Pack buffer
+  let offset = 0
+  for (const uniform of uniforms) {
+    if (typeof uniform === 'number') {
+      buffer[offset] = uniform
+      offset += 1 // leave empty space to stack primitives
+    } else {
+      offset = pad2(offset) // fill in empty space
+      buffer.set(uniform, offset)
+      offset += pad2(uniform.length)
+    }
+  }
+
+  return buffer
+}
+
 /**
  * Generates a v4 UUID. Useful for tracking unique objects.
  */
--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import { uuid } from '../src'
+import { uuid, std140, Uniform } from '../src'
 
 describe('utils/uuid', () => {
   it('is UUID RFC compliant', () => {
@@ -11,5 +11,14 @@ describe('utils/uuid', () => {
     expect(uuids.every((uuid) => uuid.match(/\d|\w/g).length === 32)).toBe(true)
     // All upper case letters
     expect(uuids.every((uuid) => !/[a-z]/.test(uuid))).toBe(true)
+  })
+})
+
+describe('utils/std140', () => {
+  it('packs into 16 byte chunks', () => {
+    const uniforms = Array.from({ length: 1000 }, (_, i) => (i < 2 ? i : Array(i).fill(0))) as unknown as Uniform[]
+    const buffer = std140(uniforms)
+
+    expect(buffer.byteLength % 16).toBe(0)
   })
 })
