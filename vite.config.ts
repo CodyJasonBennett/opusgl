7e89c1a274abbe18d9a289cdcef1de856a21b3e9
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2022-09-17 00:32:57 -0500

    chore: glob examples, setup origin trial

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   root: process.argv[2] ? undefined : 'examples',
   logLevel: process.argv[2] ? 'warn' : 'info',
+  server: { port: 80 },
   resolve: {
     alias: {
       opusgl: path.resolve(process.cwd(), 'src'),
@@ -37,6 +38,19 @@ export default defineConfig({
       name: 'vite-tsc',
       generateBundle() {
         this.emitFile({ type: 'asset', fileName: 'index.d.ts', source: `export * from '../src'` })
+      },
+      transformIndexHtml() {
+        return [
+          {
+            tag: 'meta',
+            attrs: {
+              'http-equiv': 'origin-trial',
+              content:
+                'ApB+TDs41dv4jFD67a312hCPRMkNgkhNEK6Or8SQWWNtTEqn1CbOk3tyvbbvtnNuaR2Wn4mTx8ivaDxpH+2WpAsAAABHeyJvcmlnaW4iOiJodHRwOi8vbG9jYWxob3N0OjgwIiwiZmVhdHVyZSI6IldlYkdQVSIsImV4cGlyeSI6MTY3NTIwOTU5OX0=',
+            },
+            injectTo: 'head',
+          },
+        ]
       },
     },
   ],
